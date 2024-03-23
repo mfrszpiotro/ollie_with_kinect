84ef4ba0a5b59ac79de135510cf8eee556cb783b
Marcel <marcel.piotrowski5543@gmail.com>
2024-03-23 22:35:11 +0100

    feat: usage of CircularProgressbar third-party component with gradient stroke

--- src/renderer/routes/Report/Report.tsx
+++ src/renderer/routes/Report/Report.tsx
@@ -2,11 +2,37 @@ import { Link } from 'react-router-dom';
 import comparison from '../../../../comparison.json';
 import { TimeTwoEventsComparison } from './data_interfaces';
 import TableTTE from './components/TableTTE';
+import CircleTTE from './components/CircleTTE';
+import ChartDTW from './components/ChartDTW';
 import './Report.css';
+
+export interface GradientProps {
+  startColor: string;
+  endColor: string;
+  idCSS: string;
+  rotation: string;
+}
+
+function GradientSVG({ startColor, endColor, idCSS, rotation }: GradientProps) {
+  const gradientTransform = `rotate(${rotation})`;
+
+  return (
+    <svg style={{ height: 0 }}>
+      <defs>
+        <linearGradient id={idCSS} gradientTransform={gradientTransform}>
+          <stop offset="0%" stopColor={startColor} />
+          <stop offset="100%" stopColor={endColor} />
+        </linearGradient>
+      </defs>
+    </svg>
+  );
+}
 
 export default function Report() {
   const ComparisonsTTE = comparison.TimeTwoEvents as TimeTwoEventsComparison[];
-  const ComparinsonsDTW = comparison.DynamicTimeWarps as [];
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
+  const ComparinsonsDTW = comparison.DynamicTimeWarps as []; // todo
+  const gradId = 'pink-gradient';
   return (
     <>
       <Link to="/">
@@ -15,8 +41,19 @@ export default function Report() {
         </button>
       </Link>
       <div>
+        <ChartDTW />
+        <ChartDTW />
         <TableTTE comparisonData={ComparisonsTTE[0]} />
         <TableTTE comparisonData={ComparisonsTTE[1]} />
+        <GradientSVG
+          startColor="#EC85DA"
+          endColor="#FDB6BF"
+          idCSS={gradId}
+          rotation="0"
+        />
+        <CircleTTE comparisonData={ComparisonsTTE[0]} idGradient={gradId} />
+        <CircleTTE comparisonData={ComparisonsTTE[1]} idGradient={gradId} />
+        <CircleTTE comparisonData={ComparisonsTTE[2]} idGradient={gradId} />
       </div>
       <div>{JSON.stringify(comparison)}</div>
     </>
--- src/renderer/routes/Report/components/CircleTTE.tsx
+++ src/renderer/routes/Report/components/CircleTTE.tsx
@@ -1,11 +1,24 @@
-import React from 'react';
+import { CircularProgressbar } from 'react-circular-progressbar';
 import { TimeTwoEventsComparison } from '../data_interfaces';
+import 'react-circular-progressbar/dist/styles.css';
+import './CircleTTE.css';
 
 export interface Props {
   comparisonData: TimeTwoEventsComparison;
+  idGradient: string;
 }
 
-export default function CircleTTE({ comparisonData }: Props) {
-  const { how_close } = comparisonData;
-  return <div />;
+export default function CircleTTE({ comparisonData, idGradient }: Props) {
+  const { absolute_percent } = comparisonData.how_close;
+  return (
+    <div style={{ width: 200, height: 200 }}>
+      <CircularProgressbar
+        value={absolute_percent}
+        text={absolute_percent ? `${absolute_percent.toFixed(0)}%` : 'N/A'}
+        styles={{
+          path: { stroke: `url(#${idGradient})` },
+        }}
+      />
+    </div>
+  );
 }
