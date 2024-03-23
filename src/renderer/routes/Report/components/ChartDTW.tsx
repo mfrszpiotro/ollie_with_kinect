e4c370056955e44e7d220e0f56af16bcd59d9fcd
Marcel <marcel.piotrowski5543@gmail.com>
2024-03-23 22:36:22 +0100

    feat: background setup for dtw chart style

---
+++ src/renderer/routes/Report/components/ChartDTW.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+import './ChartDTW.css';
+
+export interface Props {
+  comparisonData: any;
+}
+
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+export default function ChartDTW() {
+  return <div className="chart-dtw">test</div>;
+}
