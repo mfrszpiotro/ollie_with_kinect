75107eff49e7aecad53baf389ef984963fd29b6d
Marcel <marcel.piotrowski5543@gmail.com>
2024-04-14 20:52:49 +0200

    feat: converting body frames to process_ollie specific data

--- src/kinect_interfaces.ts
+++ src/kinect_interfaces.ts
@@ -1,7 +1,8 @@
+/* eslint-disable no-unused-vars */
 // ***
 // * Typescript interfaces for kinect2@0.2.3 npm package (https://github.com/wouterverweirder/kinect2)
 // ***
-// number/string types mostly from floats/chars with some exceptions (see: other comments)
+// NOTE: number/string types mostly from floats/chars with some exceptions (see: other comments)
 export interface Joint {
   depthX: number;
   depthY: number;
@@ -55,4 +56,117 @@ export interface MultiSourceFrame {
   body: BodyFrame;
   color: ColorFrame;
   depth: DepthFrame;
+}
+
+// helper interfaces:
+
+export enum JointType {
+  SpineBase = 0,
+  SpineMid = 1,
+  Neck = 2,
+  Head = 3,
+  ShoulderLeft = 4,
+  ElbowLeft = 5,
+  WristLeft = 6,
+  HandLeft = 7,
+  ShoulderRight = 8,
+  ElbowRight = 9,
+  WristRight = 10,
+  HandRight = 11,
+  HipLeft = 12,
+  KneeLeft = 13,
+  AnkleLeft = 14,
+  FootLeft = 15,
+  HipRight = 16,
+  KneeRight = 17,
+  AnkleRight = 18,
+  FootRight = 19,
+  SpineShoulder = 20,
+  HandTipLeft = 21,
+  ThumbLeft = 22,
+  HandTipRight = 23,
+  ThumbRight = 24,
+}
+
+export interface CsvBody {
+  Time: number;
+  SpineBase_x: number;
+  SpineBase_y: number;
+  SpineBase_z: number;
+  SpineMid_x: number;
+  SpineMid_y: number;
+  SpineMid_z: number;
+  Neck_x: number;
+  Neck_y: number;
+  Neck_z: number;
+  Head_x: number;
+  Head_y: number;
+  Head_z: number;
+  ShoulderLeft_x: number;
+  ShoulderLeft_y: number;
+  ShoulderLeft_z: number;
+  ElbowLeft_x: number;
+  ElbowLeft_y: number;
+  ElbowLeft_z: number;
+  WristLeft_x: number;
+  WristLeft_y: number;
+  WristLeft_z: number;
+  HandLeft_x: number;
+  HandLeft_y: number;
+  HandLeft_z: number;
+  ShoulderRight_x: number;
+  ShoulderRight_y: number;
+  ShoulderRight_z: number;
+  ElbowRight_x: number;
+  ElbowRight_y: number;
+  ElbowRight_z: number;
+  WristRight_x: number;
+  WristRight_y: number;
+  WristRight_z: number;
+  HandRight_x: number;
+  HandRight_y: number;
+  HandRight_z: number;
+  HipLeft_x: number;
+  HipLeft_y: number;
+  HipLeft_z: number;
+  KneeLeft_x: number;
+  KneeLeft_y: number;
+  KneeLeft_z: number;
+  AnkleLeft_x: number;
+  AnkleLeft_y: number;
+  AnkleLeft_z: number;
+  FootLeft_x: number;
+  FootLeft_y: number;
+  FootLeft_z: number;
+  HipRight_x: number;
+  HipRight_y: number;
+  HipRight_z: number;
+  KneeRight_x: number;
+  KneeRight_y: number;
+  KneeRight_z: number;
+  AnkleRight_x: number;
+  AnkleRight_y: number;
+  AnkleRight_z: number;
+  FootRight_x: number;
+  FootRight_y: number;
+  FootRight_z: number;
+  SpineShoulder_x: number;
+  SpineShoulder_y: number;
+  SpineShoulder_z: number;
+  HandTipLeft_x: number;
+  HandTipLeft_y: number;
+  HandTipLeft_z: number;
+  ThumbLeft_x: number;
+  ThumbLeft_y: number;
+  ThumbLeft_z: number;
+  HandTipRight_x: number;
+  HandTipRight_y: number;
+  HandTipRight_z: number;
+  ThumbRight_x: number;
+  ThumbRight_y: number;
+  ThumbRight_z: number;
+  Floor_x: number;
+  Floor_y: number;
+  Floor_z: number;
+  Floor_w: number;
 }
--- src/renderer/routes/Recorder/KinectCanvas.tsx
+++ src/renderer/routes/Recorder/KinectCanvas.tsx
@@ -7,7 +7,8 @@ import {
   BodyFrame,
   Body,
   MultiSourceFrame,
-  Joint,
+  CsvBody,
+  JointType,
 } from '../../../kinect_interfaces';
 
 const CANVAS_RECORDING_BPS = 5000000;
@@ -81,6 +82,158 @@ function renderBodyFrame(
   });
 }
 
+function extractCsvBody(bodyFrame: BodyFrame): CsvBody {
+  const result = {} as CsvBody;
+  const { bodies, floorClipPlane } = bodyFrame;
+  result.Floor_x = floorClipPlane.x;
+  result.Floor_y = floorClipPlane.y;
+  result.Floor_z = floorClipPlane.z;
+  result.Floor_w = floorClipPlane.w;
+  // todo: figure out how to deal with multiple bodies in frame
+  let bodyFound = null as Body | null;
+  for (let i = 0; i < bodies.length; i += 1) {
+    if (bodies[i].tracked) {
+      bodyFound = bodies[i];
+      break;
+    }
+  }
+  if (bodyFound) {
+    for (let i = 0; i < bodyFound.joints.length; i += 1) {
+      const joint = bodyFound.joints[i];
+      switch (joint.jointType) {
+        case JointType.Head:
+          result.Head_x = joint.cameraX;
+          result.Head_y = joint.cameraY;
+          result.Head_z = joint.cameraZ;
+          break;
+        case JointType.Neck:
+          result.Neck_x = joint.cameraX;
+          result.Neck_y = joint.cameraY;
+          result.Neck_z = joint.cameraZ;
+          break;
+        case JointType.SpineShoulder:
+          result.SpineShoulder_x = joint.cameraX;
+          result.SpineShoulder_y = joint.cameraY;
+          result.SpineShoulder_z = joint.cameraZ;
+          break;
+        case JointType.ShoulderLeft:
+          result.ShoulderLeft_x = joint.cameraX;
+          result.ShoulderLeft_y = joint.cameraY;
+          result.ShoulderLeft_z = joint.cameraZ;
+          break;
+        case JointType.ElbowLeft:
+          result.ElbowLeft_x = joint.cameraX;
+          result.ElbowLeft_y = joint.cameraY;
+          result.ElbowLeft_z = joint.cameraZ;
+          break;
+        case JointType.WristLeft:
+          result.WristLeft_x = joint.cameraX;
+          result.WristLeft_y = joint.cameraY;
+          result.WristLeft_z = joint.cameraZ;
+          break;
+        case JointType.HandLeft:
+          result.HandLeft_x = joint.cameraX;
+          result.HandLeft_y = joint.cameraY;
+          result.HandLeft_z = joint.cameraZ;
+          break;
+        case JointType.HandTipLeft:
+          result.HandTipLeft_x = joint.cameraX;
+          result.HandTipLeft_y = joint.cameraY;
+          result.HandTipLeft_z = joint.cameraZ;
+          break;
+        case JointType.ThumbLeft:
+          result.ThumbLeft_x = joint.cameraX;
+          result.ThumbLeft_y = joint.cameraY;
+          result.ThumbLeft_z = joint.cameraZ;
+          break;
+        case JointType.ShoulderRight:
+          result.ShoulderRight_x = joint.cameraX;
+          result.ShoulderRight_y = joint.cameraY;
+          result.ShoulderRight_z = joint.cameraZ;
+          break;
+        case JointType.ElbowRight:
+          result.ElbowRight_x = joint.cameraX;
+          result.ElbowRight_y = joint.cameraY;
+          result.ElbowRight_z = joint.cameraZ;
+          break;
+        case JointType.WristRight:
+          result.WristRight_x = joint.cameraX;
+          result.WristRight_y = joint.cameraY;
+          result.WristRight_z = joint.cameraZ;
+          break;
+        case JointType.HandRight:
+          result.HandRight_x = joint.cameraX;
+          result.HandRight_y = joint.cameraY;
+          result.HandRight_z = joint.cameraZ;
+          break;
+        case JointType.HandTipRight:
+          result.HandTipRight_x = joint.cameraX;
+          result.HandTipRight_y = joint.cameraY;
+          result.HandTipRight_z = joint.cameraZ;
+          break;
+        case JointType.ThumbRight:
+          result.ThumbRight_x = joint.cameraX;
+          result.ThumbRight_y = joint.cameraY;
+          result.ThumbRight_z = joint.cameraZ;
+          break;
+        case JointType.SpineMid:
+          result.SpineMid_x = joint.cameraX;
+          result.SpineMid_y = joint.cameraY;
+          result.SpineMid_z = joint.cameraZ;
+          break;
+        case JointType.SpineBase:
+          result.SpineBase_x = joint.cameraX;
+          result.SpineBase_y = joint.cameraY;
+          result.SpineBase_z = joint.cameraZ;
+          break;
+        case JointType.HipLeft:
+          result.HipLeft_x = joint.cameraX;
+          result.HipLeft_y = joint.cameraY;
+          result.HipLeft_z = joint.cameraZ;
+          break;
+        case JointType.KneeLeft:
+          result.KneeLeft_x = joint.cameraX;
+          result.KneeLeft_y = joint.cameraY;
+          result.KneeLeft_z = joint.cameraZ;
+          break;
+        case JointType.AnkleLeft:
+          result.AnkleLeft_x = joint.cameraX;
+          result.AnkleLeft_y = joint.cameraY;
+          result.AnkleLeft_z = joint.cameraZ;
+          break;
+        case JointType.FootLeft:
+          result.FootLeft_x = joint.cameraX;
+          result.FootLeft_y = joint.cameraY;
+          result.FootLeft_z = joint.cameraZ;
+          break;
+        case JointType.HipRight:
+          result.HipRight_x = joint.cameraX;
+          result.HipRight_y = joint.cameraY;
+          result.HipRight_z = joint.cameraZ;
+          break;
+        case JointType.KneeRight:
+          result.KneeRight_x = joint.cameraX;
+          result.KneeRight_y = joint.cameraY;
+          result.KneeRight_z = joint.cameraZ;
+          break;
+        case JointType.AnkleRight:
+          result.AnkleRight_x = joint.cameraX;
+          result.AnkleRight_y = joint.cameraY;
+          result.AnkleRight_z = joint.cameraZ;
+          break;
+        case JointType.FootRight:
+          result.FootRight_x = joint.cameraX;
+          result.FootRight_y = joint.cameraY;
+          result.FootRight_z = joint.cameraZ;
+          break;
+        default:
+          throw new Error('Unexpected joint value');
+      }
+    }
+  }
+  return result;
+}
+
 export default function KinectCanvas() {
   const canvasRef = useRef<HTMLCanvasElement>(null);
   const videoRef = useRef<HTMLVideoElement>(null);
@@ -95,7 +248,7 @@ export default function KinectCanvas() {
     const currentStartButton = startButtonRef.current as HTMLButtonElement;
     const currentStopButton = stopButtonRef.current as HTMLButtonElement;
 
-    let recordedJoints = [] as Joint[][];
+    let recordedFrames = [] as BodyFrame[];
     let chunks = [] as any;
     let recordingStartTime = 0;
     const mediaRecorder = new MediaRecorder(streamCanvas, {
@@ -105,6 +258,9 @@ export default function KinectCanvas() {
       chunks.push(e.data);
     };
     mediaRecorder.onstop = async () => {
+      const recordedCsvBodies = recordedFrames.map((bodyFrame) =>
+        extractCsvBody(bodyFrame),
+      );
       const videoBlob = new Blob(chunks, { type: 'video/mp4' });
       chunks = [];
       const videoBuffer = Buffer.from(await videoBlob.arrayBuffer());
@@ -112,16 +268,16 @@ export default function KinectCanvas() {
         console.log(`Your file has been saved to ${recordingStartTime}.mp4`);
       });
       const filenameJson = `${recordingStartTime}.json`;
-      const framesToJsonString = JSON.stringify(recordedJoints);
+      const framesToJsonString = JSON.stringify(recordedCsvBodies);
       fs.writeFile(filenameJson, framesToJsonString, 'utf8', () => {
         console.log(`Your file has been saved to ${filenameJson}`);
       });
       const filenameCsv = `${recordingStartTime}.csv`;
-      const framesToCsvString = json2csv(recordedJoints);
+      const framesToCsvString = json2csv(recordedCsvBodies);
       fs.writeFile(filenameCsv, framesToCsvString, 'utf8', () => {
         console.log(`Your file has been saved to ${filenameCsv}`);
       });
-      recordedJoints = [];
+      recordedFrames = [];
       recordingStartTime = 0;
       const videoURL = URL.createObjectURL(videoBlob);
       currentVideo.src = videoURL;
@@ -152,11 +308,7 @@ export default function KinectCanvas() {
         if (frame.body) {
           renderBodyFrame(context, context.canvas, frame.body, false);
           if (mediaRecorder.state === 'recording') {
-            // eslint-disable-next-line no-restricted-syntax
-            const { bodies } = frame.body;
-            for (let i = 0; i < frame.body.bodies.length; i += 1) {
-              if (bodies[i].tracked) recordedJoints.push(bodies[i].joints);
-            }
+            recordedFrames.push(frame.body);
           }
         }
       });
